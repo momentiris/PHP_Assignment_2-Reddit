c6fb4545dab883e8039cd4749ddef26258e1523b
Andreas Lundqvist <31646645+momentiris@users.noreply.github.com>
2017-12-25 16:29:45 +0100

    managed to send data to php vote auth app votes.php and get return message to js via fetch POST

--- app/auth/votes.php
+++ app/auth/votes.php
@@ -5,6 +5,11 @@ require __DIR__.'/../autoload.php';
 
 <?php
 
-if (isset($_POST)) {
-  var_dump($_POST);
+if (isset($_POST['upvote'])) {
+  echo json_encode("hej");
 }
+// $sId = $_SESSION['user']['id'];
+// var_dump($sId);
+// $votes = $pdo->prepare('SELECT title, content, url, time, votes username FROM posts');
+// $votes->execute();
+// $votes = $votes->fetchAll(PDO::FETCH_ASSOC);
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,7 @@
-let upVote = 0;
-let downVote = 0;
+
+let downVoted = 0;
+
+
 let pageNum = 1;
 let totalPosts = 0;
 
@@ -9,30 +11,33 @@ const postContainer = document.querySelector('.postcontainer');
 let divElement = document.createElement('div');
 let voteButton = document.createElement('button');
 voteButton.classList.add('upvote');
-voteButton.innerText = "upvote";
 
-const voteFunc = () => {
-upVote++;
+voteButton.innerText = "upvotes";
+voteButton.dataset.dir = "1";
+voteButton.setAttribute("value", ``);
 
-fetch(voteApi, {
-    method: 'POST',
 
-     body: JSON.stringify({
-    name: 'dean',
-    login: 'dean',
-  })
-})
-.then(function (data) {
-  console.log('Request success: ', data);
-})
-.catch(function (error) {
-  console.log('Request failure: ', error);
-});
+const voteFunc = (e) => {
+  fetch(voteApi, {
+      method: "POST",
+      headers: {"Content-Type": "application/x-www-form-urlencoded"},
+      credentials: "include",
+      body: `upvote=${e.target.dataset.value}&dir=${e.target.dataset.dir}`
+    })
+    .then(response => {
+      console.log(response.json());
+    })
+
+
+
+console.log(e.target.dataset.dir);
+console.log(e.target.value);
+
 
 }
 
-const voteBtn = (voteBtn) => {
-  voteBtn.addEventListener('click', voteFunc);
+const upvote = (upvote) => {
+  upvote.addEventListener('click', voteFunc);
 }
 
 
@@ -44,7 +49,9 @@ fetch(`${api}/?page=${page}`)
   .then(postsOnPage => {
     totalPosts = JSON.parse(postsOnPage['total'][0]);
     for (posts of postsOnPage['posts'][0]) {
-      let postElement =  `<div class="card postcontainer" style="margin-bottom: 1rem;padding: 0;">
+      voteButton.setAttribute("value", `${posts.author_id}`);
+      let postElement =
+      `<div class="card postcontainer" style="margin-bottom: 1rem;padding: 0;">
         <div class="card-body">
           <h5 class="card-title title">${posts.title}</h4>
           <p class="card-text small content">${posts.content}</p>
@@ -53,45 +60,25 @@ fetch(`${api}/?page=${page}`)
           <p class="card-text small time">${posts.time}</p>
           <p class="card-text small votes">${posts.votes}</p>
         </div>
-        </div>`;
-
-
-
-
-
+      </div>`;
           divElement.innerHTML += postElement;
           postContainer.appendChild(divElement);
           divElement.appendChild(voteButton);
+    };
 
-          };
-          const upvote = document.querySelectorAll('.upvote');
-          upvote.forEach(voteBtn);
-    //       for (button of upvote) {
-    //         button.addEventListener('click', function(){
-    //           console.log('hej');
-    //         })
-    //
-    // }
+          const upvoteArr = document.querySelectorAll('.upvote');
+          Array.from(upvoteArr).forEach(upvote);
+
 })
-
 }
 
 getPage(pageNum);
-const clicked = () => {
-  console.log('hej');
-}
 
-
-
-
-
-  window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function() {
     let currentAmountOfPosts = postContainer.querySelectorAll('.card').length;
     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight && currentAmountOfPosts < totalPosts){
       console.log(currentAmountOfPosts, totalPosts);
       pageNum++;
         getPage(pageNum);
-        console.log('hej');
-
-    }
-  });
+  }
+});
